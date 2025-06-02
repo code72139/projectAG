035ceb7fb40943e9879e403b81c430a9559819e8
code72139 <joserochaherp@gmail.com>
2025-06-02 13:34:34 -0500

    config. programcs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ using Microsoft.AspNetCore.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? 
+    builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllersWithViews();
 
